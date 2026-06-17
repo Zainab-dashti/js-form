096cb40a5d0c3aa7e94f63b5bdb373e62cb78015
Zainab-dashti <zainuudashti@gmail.com>
2026-06-17 15:26:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function studForm(){
+
+let name = document.getElementById('name').value;
+let fname = document.getElementById('fname').value;
+let age = document.getElementById('age').value;
+let gender = document.getElementById('gender').value;
+let rollno = document.getElementById('rollno').value;
+let email = document.getElementById('email').value;
+let phone = document.getElementById('phone').value;
+let address = document.getElementById('address').value;
+let course = document.getElementById('course').value;
+
+document.write('<font face="algerain" color="orange" size="20">Student Form Output: </font> <br><br><br><br>');
+document.write('<b>Name:</b>',name,'<br>');
+document.write('<b> Father Name:</b>',fname,'<br>');
+document.write('<b>Age:</b>',age,'<br>');
+document.write('<b>Gender:</b>',gender,'<br>');
+document.write('<b>Roll No:</b>',rollno,'<br>');
+document.write('<b>Email:</b>',email,'<br>');
+document.write('<b>phone no:</b>',phone,'<br>');
+document.write('<b>address:</b>',address,'<br>');
+document.write('<b>Course:</b>',course,'<br>');
+}
+
+
+
+
